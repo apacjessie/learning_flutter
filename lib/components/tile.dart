8e7ca8a71e3aad6ec5a7653b82afb9bdec71fa29
apacjessie <apacjessie@gmail.com>
2023-12-09 22:31:36 +0800

    Update: Add Sizedbox

--- lib/components/tile.dart
+++ lib/components/tile.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
-CheckboxListTile tile(index, task, isComplete, toggleTaskCompletion) {
-  return CheckboxListTile(
-    value: isComplete,
-    onChanged: (_) => toggleTaskCompletion(index),
-    activeColor: Colors.blue,
-    checkColor: Colors.white,
-    tileColor:
-        isComplete ? Colors.blue.withOpacity(0.8) : Colors.red.withOpacity(0.8),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(5.0),
-    ),
-    secondary: isComplete
-        ? const Icon(Icons.check, color: Colors.white)
-        : const Icon(Icons.close),
-    title: Text(
-      task,
-      style: TextStyle(color: isComplete ? Colors.white : Colors.black),
-    ),
-  );
+Dismissible tile(index, task, isComplete, toggleTaskCompletion, deleteTask) {
+  return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (_) => deleteTask(index),
+      child: Column(
+        children: [
+          CheckboxListTile(
+            value: isComplete,
+            onChanged: (_) => toggleTaskCompletion(index),
+            activeColor: Colors.blue,
+            checkColor: Colors.white,
+            tileColor: isComplete
+                ? Colors.blue.withOpacity(0.8)
+                : Colors.red.withOpacity(0.8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            secondary: isComplete
+                ? const Icon(Icons.check, color: Colors.white)
+                : const Icon(Icons.close),
+            title: Text(
+              task,
+              style: TextStyle(color: isComplete ? Colors.white : Colors.black),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+      ));
 }
